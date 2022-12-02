7f24489aea4f1eefd5d9474bb76823f48c6cbb12
djr- <drewjryder@gmail.com>
2022-12-01 17:39:13 -0800

    day1

---
+++ day1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+string[] lines = File.ReadAllLines("input.txt");
+
+var maxElfCalorieCount = 0;
+
+var currElfCalorieCount = 0;
+foreach (var line in lines)
+{
+    if (string.IsNullOrEmpty(line))
+    {
+        currElfCalorieCount = 0;
+        continue;
+    }
+    
+    currElfCalorieCount += Int32.Parse(line);
+    maxElfCalorieCount = Math.Max(maxElfCalorieCount, currElfCalorieCount);
+}
+
+Console.WriteLine(maxElfCalorieCount);
